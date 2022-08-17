d161586a8dff194e5bb6ca82f710640884b38402
Axel <axelalvarenga97@gmail.com>
2022-08-16 23:33:30 -0400

    admin

--- Eldeportista/app/core/erp/admin.py
+++ Eldeportista/app/core/erp/admin.py
@@ -1,3 +1,6 @@
+from unicodedata import category
 from django.contrib import admin
-
+from core.erp.models import *
 # Register your models here.
+
+admin.site.register(Category)
--- Eldeportista/app/core/erp/models.py
+++ Eldeportista/app/core/erp/models.py
@@ -17,6 +17,15 @@ class empleado(models.Model):
         ordering=['id']
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=150,verbose_name='Nombre',unique=True)
+    def __str__(self):
+         return self.name
+
+    class Meta:
+         verbose_name= 'Categoria'
+         verbose_name_plural='Categorias'
+         ordering=['id']
     
 
 

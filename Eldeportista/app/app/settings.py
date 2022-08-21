dde83f612f91e64256d1103f313ae77ff7fa48cd
Axel <axelalvarenga97@gmail.com>
2022-08-21 18:31:55 -0400

    solucion basedir

--- Eldeportista/app/app/settings.py
+++ Eldeportista/app/app/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
+    
 ]
 
 # Default primary key field type
--- Eldeportista/app/app/urls.py
+++ Eldeportista/app/app/urls.py
@@ -19,5 +19,5 @@ from core.erp.views import vista1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
+    path('inicio/', vista1)
 ]

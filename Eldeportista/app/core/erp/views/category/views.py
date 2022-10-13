04e4e1faf285c0021eb98dbfa71613339002b685
Axel <axelalvarenga97@gmail.com>
2022-10-12 22:59:29 -0300

    Update hecho

--- Eldeportista/app/core/erp/views/category/views.py
+++ Eldeportista/app/core/erp/views/category/views.py
@@ -1,6 +1,6 @@
 
 
-from django.views.generic import ListView , CreateView
+from django.views.generic import ListView , CreateView , UpdateView
 from core.erp.forms import ListForm
 from core.erp.models import producto
 from django.urls import reverse_lazy
@@ -26,4 +26,15 @@ class CreateListView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Cargar Productos'
+        return context
+
+class UpdateListView(UpdateView):
+    model = producto
+    form_class = ListForm
+    template_name = 'core/erp/templates/producto/create.html'
+    success_url = reverse_lazy('erp:producto_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Editar Productos'
         return context

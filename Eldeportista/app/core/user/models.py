4f1c465e30cb67c045f239850cd875ff5ccb75e9
axel <axelalvarenga97@gmail.com>
2023-01-04 13:29:33 -0300

    Listado de usuarios

--- Eldeportista/app/core/user/models.py
+++ Eldeportista/app/core/user/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.forms import model_to_dict
 # Create your models here.
 
 class User(AbstractUser):
     ci = models.CharField(max_length=150, verbose_name='Cedula', unique=True)
+
+    def toJSON(self):
+        item = model_to_dict(self, exclude=['password', 'groups', 'user_permissions', 'last_login'])
+        if self.last_login:
+            item['last_login'] = self.last_login.strftime('%Y-%m-%d')
+            item['date_joined'] = self.date_joined.strftime('%Y-%m-%d')
+        return item
